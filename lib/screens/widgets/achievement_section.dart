7410e42e2cfc314c928daf2081c4196a793139f0
reenolesigues <reeno.lesigues@gmail.com>
2021-04-18 13:53:09 +0800

    clean up achievement section

--- lib/screens/widgets/achievement_section.dart
+++ lib/screens/widgets/achievement_section.dart
@@ -1,178 +1,99 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'dart:math' as math;
-
 import 'package:reeno_website/utils/section_icon_header.dart';
 
 class AchievementSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CollapsingList();
-  }
-}
-
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  _SliverAppBarDelegate({
-    @required this.minHeight,
-    @required this.maxHeight,
-    @required this.child,
-  });
-  final double minHeight;
-  final double maxHeight;
-  final Widget child;
-  @override
-  double get minExtent => minHeight;
-  @override
-  double get maxExtent => math.max(maxHeight, minHeight);
-  @override
-  Widget build(
-      BuildContext context,
-      double shrinkOffset,
-      bool overlapsContent)
-  {
-    return new SizedBox.expand(child: child);
-  }
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
-    return maxHeight != oldDelegate.maxHeight ||
-        minHeight != oldDelegate.minHeight ||
-        child != oldDelegate.child;
-  }
-}
-
-class CollapsingList extends StatelessWidget {
-  SliverPersistentHeader makeHeader(String headerText) {
-    return SliverPersistentHeader(
-      pinned: true,
-      delegate: _SliverAppBarDelegate(
-        minHeight: 60.0,
-        maxHeight: 100.0,
+    return Scrollbar(
+      child: SingleChildScrollView(
         child: Container(
-            color: Color(0xFF151026),
-            child: Center(
-              child: FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Text(headerText, style: TextStyle(fontSize: 20, color: Colors.white54)),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SectionIconHeader(sectionName: "Certifications", iconData: Icons.folder_open),
+                    SizedBox(height: 40,),
+                    renderCertification("Certified Lean Six Sigma Yellowbelt (CLSSYB)", "Nov 2020", "Six Sigma"),
+                    SizedBox(height: 20,),
+                    renderCertification("Certified Web Application Security Professional (CWASP)", "Feb 2020", "Network Intelligence"),
+                    SizedBox(height: 20,),
+                    renderCertification("Certified Payment Security Practitioner (CPSP)", "Feb 2020", "Network Intelligence"),
+                    SizedBox(height: 20,),
+                    renderCertification("Philippine National IT Standards", "April 2013", "Philippine National IT Standards"),
+                  ],
+                ),
               ),
-            )
+              Divider(thickness: 2, color: Colors.white70, indent: 200, endIndent: 200, height: 50,),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SectionIconHeader(sectionName: "Awards", iconData: Entypo.trophy),
+                    SizedBox(height: 40,),
+                    renderCertification("Homecredit Global No-Glitch Hackathon - Grand Champion", "April 2021", "Home Credit International"),
+                    SizedBox(height: 20,),
+                    renderCertification("F.I.R.E. Awardee", "October 2019", "Homecredit Philippines"),
+                    SizedBox(height: 20,),
+                    renderCertification("Jjang Awardee", "September 2014", "Samsung R&D Philippines"),
+                    SizedBox(height: 20,),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-  @override
-  Widget build(BuildContext context) {
-    return Scrollbar(
-      child: Row(
-        children: [
-          Expanded(
-            child: CustomScrollView(
-              slivers: <Widget>[
-                SliverAppBar(
-                  title: SectionIconHeader(sectionName: "Certifications", iconData: Icons.folder_open),
-                  backgroundColor: Colors.transparent,
-                  pinned: true,
-                  centerTitle: true,
-                ),
-                makeHeader('Techincal'),
-                SliverList(
-                  delegate: SliverChildListDelegate(
-                    [
-                      Container(color: Colors.cyan, height: 150.0),
-                      Container(color: Colors.indigo, height: 150.0),
-                      Container(color: Colors.blue, height: 150.0),
-                    ],
-                  ),
-                ),
-                makeHeader('Soft Skills'),
-                SliverFixedExtentList(
-                  itemExtent: 150.0,
-                  delegate: SliverChildListDelegate(
-                    [
-                      Container(color: Colors.cyan, height: 150.0),
-                      Container(color: Colors.indigo, height: 150.0),
-                      Container(color: Colors.blue, height: 150.0),
-                    ],
-                  ),
-                ),
-                makeHeader('Company'),
-                SliverToBoxAdapter(
-                  child: Container(
-                    height: 1000,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return Row(
-                          children: [
-                            Container(color: Colors.red, height: 1000.0, width: 300.0,),
-                            Container(color: Colors.black38, height: 1000.0, width: 300.0,),
-                            Container(color: Colors.green, height: 1000.0, width: 300.0,),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                ),
-                /*Last Row for LinkedIn/FB connect*/
-              ],
-            ),
-            flex: 1,
-          ),
-          Expanded(
-            child: CustomScrollView(
-              slivers: <Widget>[
-                SliverAppBar(
-                  title: SectionIconHeader(sectionName: "Awards", iconData: Icons.folder_open),
-                  backgroundColor: Colors.transparent,
-                  pinned: true,
-                  centerTitle: true,
-                ),
-                makeHeader('Techincal'),
-                SliverList(
-                  delegate: SliverChildListDelegate(
-                    [
-                      Container(color: Colors.cyan, height: 150.0),
-                      Container(color: Colors.indigo, height: 150.0),
-                      Container(color: Colors.blue, height: 150.0),
-                    ],
-                  ),
-                ),
-                makeHeader('Soft Skills'),
-                SliverFixedExtentList(
-                  itemExtent: 150.0,
-                  delegate: SliverChildListDelegate(
-                    [
-                      Container(color: Colors.cyan, height: 150.0),
-                      Container(color: Colors.indigo, height: 150.0),
-                      Container(color: Colors.blue, height: 150.0),
-                    ],
-                  ),
-                ),
-                makeHeader('Company'),
-                SliverToBoxAdapter(
-                  child: Container(
-                    height: 1000,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return Row(
-                          children: [
-                            Container(color: Colors.red, height: 1000.0, width: 300.0,),
-                            Container(color: Colors.black38, height: 1000.0, width: 300.0,),
-                            Container(color: Colors.green, height: 1000.0, width: 300.0,),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                ),
-                /*Last Row for LinkedIn/FB connect*/
-              ],
-            ),
-            flex: 1,
-          )
-        ],
+}
+
+Widget renderCertification(String certification, String date, String provider) {
+  return Container(
+    height: 100,
+    width: 500,
+    decoration: BoxDecoration(
+      border: Border.all(
+        color: Colors.white10,
       ),
-    );
-  }
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+    ),
+    padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        AutoSizeText(certification,
+          maxLines: 1,
+          overflow: TextOverflow.fade,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          softWrap: true,
+          minFontSize: 6,
+          maxFontSize: 16,
+        ),
+        AutoSizeText(date,
+          maxLines: 1,
+          overflow: TextOverflow.fade,
+          style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
+          minFontSize: 6,
+          maxFontSize: 12,
+        ),
+        AutoSizeText(provider,
+          maxLines: 1,
+          overflow: TextOverflow.fade,
+          style: TextStyle(color: Colors.white),
+          minFontSize: 6,
+          maxFontSize: 12,
+        ),
+      ],
+    ),
+  );
 }
